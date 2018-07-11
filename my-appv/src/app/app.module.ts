1c11dc08e817de184583bae9348f3cfbbd2c4179
Dennis <dreyes53@terpmail.umd.edu>
2018-07-10 22:21:39 -0400

    Single text box test

--- my-appv/src/app/app.module.ts
+++ my-appv/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { KeyUpComponent } from './keyup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    KeyUpComponent
   ],
   imports: [
     BrowserModule
