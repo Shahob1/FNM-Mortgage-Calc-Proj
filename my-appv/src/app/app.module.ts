0bdbf4ac365446bf36ebf738513b64c58a262101
Dennis Reyes <dennisreyes@Denniss-MacBook-Pro.local>
2018-07-14 17:12:28 -0400

    small fix

--- my-appv/src/app/app.module.ts
+++ my-appv/src/app/app.module.ts
@@ -1,21 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'
 
- import { AppComponent } from './app.component';
- import { CalcComponent } from './calculator.component';
--import { FormUploadComponent } from './form-upload.component';
--import { UploadFileService } from './upload-file.service';
- 
- @NgModule({
-   declarations: [
-     AppComponent,
-     CalcComponent,
--    FormUploadComponent
-   ],
-   imports: [
-     BrowserModule,
-     FormsModule
-   ],
--  providers: [UploadFileService],
-+  providers: [],
-   bootstrap: [AppComponent]
- })
- export class AppModule { }
+import { AppComponent } from './app.component';
+import { CalcComponent } from './calculator.component';
+import { FormUploadComponent } from './form-upload.component';
+import { UploadFileService } from './upload-file.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CalcComponent,
+    FormUploadComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [UploadFileService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
