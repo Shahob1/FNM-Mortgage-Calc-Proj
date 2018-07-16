e371bc2b4950509e57006095564ae2047eee0a86
belayma <40894703+belayma@users.noreply.github.com>
2018-07-16 14:08:54 -0400

    updated with hardcoded input values
    outputs to console 
    still does not read in values from form
    still does not output to output box

--- my-appv/src/app/app.component.ts
+++ my-appv/src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+import { Calculator } from './calculator';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,39 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Mortgage Calculator';
+  @Input('interestRate') interestRate: number;
+  @Input('loanAmount') loan: any;
+  @Input('loanTerm') years: any;
+  @Input('interestRate') interest: any;
+  @Input('output') output: any;
+  
+  // mortgageCalc = new Calculator(400000, 20000, 4, 30);
+
+  test = new Calculator(this.loan, 20000, this.interest, this.years);
+
+calculateTest(){
+  // console.log("INPUT:");
+  // console.log("Loan: $400,000");
+  // console.log("Downpayment: $20,000");
+  // console.log("Interest Rate: 4.00%");
+  // console.log("Years: 30");
+  // console.log("==============================");
+  // console.log("OUTPUT:");
+  // console.log("Monthly Mortgage= $" + this.mortgageCalc.calculateMortgage());
+  // console.log("Total Interest=  $" + this.mortgageCalc.calculateInterest());
+  // console.log("Total Cost=  $" + this.mortgageCalc.calculateTotal());
+
+  console.log("INPUT:");
+  console.log("Loan: $400,000");
+  console.log("Downpayment: $20,000");
+  console.log("Interest Rate: 4.00%");
+  console.log("Years: 30");
+  console.log("==============================");
+  console.log("OUTPUT:");
+  console.log("Monthly Mortgage= $" + this.test.calculateMortgage());
+  console.log("Total Interest=  $" + this.test.calculateInterest());
+  console.log("Total Cost=  $" + this.test.calculateTotal());
+
+}
+
 }
