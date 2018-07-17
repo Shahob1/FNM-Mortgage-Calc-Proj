e500a8347b2d54dbb2891a7e6e909ecce95498bf
Shouvik <shouvik.chakraborty95@gmail.com>
2018-07-17 15:47:23 -0400

    local csv created

--- my-appv/src/app/calculator.component.ts
+++ my-appv/src/app/calculator.component.ts
@@ -1,6 +1,8 @@
 /* tslint:disable:class-name component-class-suffix */
-import { Component } from '@angular/core';
-
+import { Component, AfterContentInit } from '@angular/core';
+import * as d3 from 'd3';
+import * as crossfilter from 'crossfilter2';
+import * as dc from 'dc';
 import { Calculator } from './calculator';
 
 @Component({
@@ -8,7 +10,7 @@ import { Calculator } from './calculator';
   templateUrl: './calc.component.html'
 
 })
-export class CalcComponent {
+export class CalcComponent implements AfterContentInit{
     mortgageCalc = new Calculator(400000, 0.2, .0035, 30)
 
     submitted = false;
@@ -18,5 +20,33 @@ export class CalcComponent {
     get diagnostic() {
         return JSON.stringify(this.mortgageCalc);
     }
+
+    ngAfterContentInit(): void {
+        var chart = dc.seriesChart("#time-series");
+
+        d3.csv("amort.csv").then(function(data) {
+            var format = d3.time.format("%B  %Y");
+            data.forEach(function(d) {
+                d.date = format(d.date);
+            });
+            var ndx = crossfilter(data);
+            var dimension = ndx.dimension(function(d) {return d['Date']; });
+            var group = dimension.group().reduceSum(function(d) { return +d['Interest']; });
+
+            chart
+               .width(800)
+               .height(300)
+               .brushOn(false)
+               .yAxisLabel("Dollars")
+               .xAxisLabel("Date")
+               .dimension(dimension)
+               .group(group)
+            chart.render();
+
+
+        });
+    }
+
+    
 
 }
